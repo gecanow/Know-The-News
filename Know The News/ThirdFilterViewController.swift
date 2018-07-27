bc466689b82daa63b65a3bf1f9e62f8d3eec62fc
Gaby Ecanow <gecanow@gmail.com>
2018-07-26 19:54:18 -0500

    added tableview functionality

--- Know The News/ThirdFilterViewController.swift
+++ Know The News/ThirdFilterViewController.swift
@@ -16,12 +16,51 @@ class ThirdFilterViewController: UIViewController, UITableViewDelegate, UITableV
     
     var articles = [[String: String]]()
     
+    @IBOutlet weak var tableView: UITableView!
+    
     //=========================================
     // VIEW DID LOAD
     //=========================================
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.titleTextAttributes = titleAttributes
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let query = "https://newsapi.org/v2/sources?apiKey=\(apiKey)"
+        DispatchQueue.global(qos: .userInitiated).async {
+            [unowned self] in
+            // rest of method goes here
+            
+            if let url = URL(string: query) {
+                if let data = try? Data(contentsOf: url) {
+                    let json = try! JSON(data: data)
+                    if json["status"] == "ok" {
+                        self.parseForSources(json: json)
+                        return
+                    }
+                }
+            }
+            self.loadError(problem: "There was a problem loading the news feed.")
+        }
+    }
+    
+    func parseForSources(json: JSON) {
+        for result in json["sources"].arrayValue {
+            let name = result["name"].stringValue
+            let category = result["category"].stringValue
+            let url = result["url"].stringValue
+            let language = result["language"].stringValue
+            let country = result["country"].stringValue
+            
+            let source = ["name": name, "category": category, "url": url, "language": language, "country": country]
+            allSources.append(source)
+        }
+        searchedSources = allSources
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,16 +68,26 @@ class ThirdFilterViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "yourcellIdentifire", for: indexPath) as! YourCellClass
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        let index = indexPath.row
         
         //here is programatically switch make to the table view
         let switchView = UISwitch(frame: .zero)
         switchView.setOn(false, animated: true)
-        switchView.tag = indexPath.row // for detect which row switch Changed
+        switchView.tag = index // for detect which row switch Changed
         switchView.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
         cell.accessoryView = switchView
         
+        let title = searchedSources[index]["name"]
+        let subtitle = searchedSources[index]["type"] //?
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = subtitle
+        
         return cell
+    }
+    
+    @objc func switchChanged(_ sender: UISwitch) {
+        print(sender.tag)
     }
     
     @IBAction func onTappedBegin(_ sender: Any) {
@@ -65,7 +114,6 @@ class ThirdFilterViewController: UIViewController, UITableViewDelegate, UITableV
     
     func setAndSearchQuery() {
         let query = "https://newsapi.org/v2/everything?"
-        print(query)
         
         DispatchQueue.global(qos: .userInitiated).async {
             [unowned self] in
